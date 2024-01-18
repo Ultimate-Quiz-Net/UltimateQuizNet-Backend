7da44198b4de18ea7cec028ecd71b2607db1b6f4
song_woojin <ujins8201@gmail.com>
2024-01-18 12:46:06 +0900

    fix: accessToken updated

--- src/middlewares/member.middleware.js
+++ src/middlewares/member.middleware.js
@@ -7,62 +7,22 @@ import {
 
 export default async function (req, res, next) {
   try {
-    const { accessToken, refreshToken } = req.cookies;
+    const accessToken = req.headers.authorization;
 
-    if (!accessToken || !refreshToken) {
+    if (!accessToken) {
       throw new Error(" 로그인이 필요한 서비스 입니다. ");
     }
 
     const verifyAccessToken = validateAccesstoken(accessToken);
 
     if (verifyAccessToken == "invalid token") {
-      res.clearCookie("accessToken");
-      res.clearCookie("refreshToken");
       return res
         .status(401)
         .json({ message: "토큰이 인증에 실패 하였습니다." });
     }
 
     if (verifyAccessToken == "jwt expired") {
-      const decodedInfo = decodedAccessToken(accessToken);
-
-      const member = await prisma.Members.findFirst({
-        where: { username: decodedInfo.username },
-      });
-
-      if (!member) {
-        res.clearCookie("accessToken");
-        res.clearCookie("refreshToken");
-        throw new Error(" 토큰 사용자가 존재 하지 않습니다. ");
-      }
-
-      const verifyRefreshToken = validateRefreshToken(
-        refreshToken,
-        member.hashedRefreshToken
-      );
-
-      if (verifyRefreshToken == "invalid token") {
-        res.clearCookie("accessToken");
-        res.clearCookie("refreshToken");
-        return res
-          .status(401)
-          .json({ message: "토큰이 인증에 실패 하였습니다." });
-      }
-
-      if (verifyRefreshToken == "jwt expired") {
-        res.clearCookie("accessToken");
-        res.clearCookie("refreshToken");
-        throw new Error(" 로그인이 필요한 서비스 입니다. ");
-      }
-
-      const myNewAccessToken = createAccessToken(decodedInfo.username);
-      res.cookie("accessToken", `Bearer ${myNewAccessToken}`, {
-        secure: true,
-      });
-
-      req.member = member;
-
-      next();
+      return res.status(401).json({ message: "토큰이 만료 되었습니다." });
     }
 
     const username = verifyAccessToken.username;
@@ -72,44 +32,116 @@ export default async function (req, res, next) {
     });
 
     if (!member) {
-      res.clearCookie("accessToken");
-      res.clearCookie("refreshToken");
       throw new Error(" 토큰 사용자가 존재 하지 않습니다. ");
-    }
-
-    const verifyRefreshToken = validateRefreshToken(
-      refreshToken,
-      member.hashedRefreshToken
-    );
-
-    if (verifyRefreshToken == "invalid token") {
-      res.clearCookie("accessToken");
-      res.clearCookie("refreshToken");
-      return res
-        .status(401)
-        .json({ message: "토큰이 인증에 실패 하였습니다." });
-    }
-
-    if (verifyRefreshToken == "jwt expired") {
-      const myNewRefreshToken = createRefreshToken(member.username);
-      res.cookie("refreshToken", `Bearer ${myNewRefreshToken}`, {
-        secure: true,
-      });
-
-      const salt = bcrypt.genSaltSync(parseInt(process.env.BCRYPT_SALT));
-      const hashedRefreshToken = bcrypt.hashSync(myNewRefreshToken, salt);
-
-      await prisma.Members.update({
-        where: { username },
-        data: {
-          hashedRefreshToken: hashedRefreshToken,
-        },
-      });
     }
 
     req.member = member;
 
     next();
+
+    // const { accessToken, refreshToken } = req.cookies;
+
+    // if (!accessToken || !refreshToken) {
+    //   throw new Error(" 로그인이 필요한 서비스 입니다. ");
+    // }
+
+    // const verifyAccessToken = validateAccesstoken(accessToken);
+
+    // if (verifyAccessToken == "invalid token") {
+    //   res.clearCookie("accessToken");
+    //   res.clearCookie("refreshToken");
+    //   return res
+    //     .status(401)
+    //     .json({ message: "토큰이 인증에 실패 하였습니다." });
+    // }
+
+    // if (verifyAccessToken == "jwt expired") {
+    //   const decodedInfo = decodedAccessToken(accessToken);
+
+    //   const member = await prisma.Members.findFirst({
+    //     where: { username: decodedInfo.username },
+    //   });
+
+    //   if (!member) {
+    //     res.clearCookie("accessToken");
+    //     res.clearCookie("refreshToken");
+    //     throw new Error(" 토큰 사용자가 존재 하지 않습니다. ");
+    //   }
+
+    //   const verifyRefreshToken = validateRefreshToken(
+    //     refreshToken,
+    //     member.hashedRefreshToken
+    //   );
+
+    //   if (verifyRefreshToken == "invalid token") {
+    //     res.clearCookie("accessToken");
+    //     res.clearCookie("refreshToken");
+    //     return res
+    //       .status(401)
+    //       .json({ message: "토큰이 인증에 실패 하였습니다." });
+    //   }
+
+    //   if (verifyRefreshToken == "jwt expired") {
+    //     res.clearCookie("accessToken");
+    //     res.clearCookie("refreshToken");
+    //     throw new Error(" 로그인이 필요한 서비스 입니다. ");
+    //   }
+
+    //   const myNewAccessToken = createAccessToken(decodedInfo.username);
+    //   res.cookie("accessToken", `Bearer ${myNewAccessToken}`, {
+    //     secure: true,
+    //   });
+
+    //   req.member = member;
+
+    //   next();
+    // }
+
+    // const username = verifyAccessToken.username;
+
+    // const member = await prisma.Members.findFirst({
+    //   where: { username },
+    // });
+
+    // if (!member) {
+    //   res.clearCookie("accessToken");
+    //   res.clearCookie("refreshToken");
+    //   throw new Error(" 토큰 사용자가 존재 하지 않습니다. ");
+    // }
+
+    // const verifyRefreshToken = validateRefreshToken(
+    //   refreshToken,
+    //   member.hashedRefreshToken
+    // );
+
+    // if (verifyRefreshToken == "invalid token") {
+    //   res.clearCookie("accessToken");
+    //   res.clearCookie("refreshToken");
+    //   return res
+    //     .status(401)
+    //     .json({ message: "토큰이 인증에 실패 하였습니다." });
+    // }
+
+    // if (verifyRefreshToken == "jwt expired") {
+    //   const myNewRefreshToken = createRefreshToken(member.username);
+    //   res.cookie("refreshToken", `Bearer ${myNewRefreshToken}`, {
+    //     secure: true,
+    //   });
+
+    //   const salt = bcrypt.genSaltSync(parseInt(process.env.BCRYPT_SALT));
+    //   const hashedRefreshToken = bcrypt.hashSync(myNewRefreshToken, salt);
+
+    //   await prisma.Members.update({
+    //     where: { username },
+    //     data: {
+    //       hashedRefreshToken: hashedRefreshToken,
+    //     },
+    //   });
+    // }
+
+    // req.member = member;
+
+    // next();
   } catch (error) {
     res.clearCookie("accessToken");
     res.clearCookie("refreshToken");
@@ -160,10 +192,10 @@ async function validateRefreshToken(refreshToken, hashedRefreshToken) {
 }
 
 async function decodedAccessToken(accessToken) {
-  try{
-  const [tokenType, token] = refreshToken.split(" ");
-  return jwt.decode(token, process.env.JWT_ACCESS_SECRET_KEY);
-  } catch(error) {
-    return error.message
+  try {
+    const [tokenType, token] = refreshToken.split(" ");
+    return jwt.decode(token, process.env.JWT_ACCESS_SECRET_KEY);
+  } catch (error) {
+    return error.message;
   }
 }
